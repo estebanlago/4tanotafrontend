afdf67a7c98e427228b04a597ff6853c9bf0501d
estebanlago <eliazar.lagos@inacapmail.cl>
2024-07-24 06:11:50 -0400

    uu

--- src/components/Nota.js
+++ src/components/Nota.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function Nota({ nota }) {
+function Nota({ nota, eliminar }) {
   const { titulo, descripcion, importante } = nota;
   return (
     <div class={`card ${importante ? 'bt-importante' : 'bt-no-importante'} text-black`}>
       <div class="card-body">
         <h4 class="card-title">{titulo}</h4>
         <p class="card-text">{descripcion}</p>
+        <button class="btn btn-secondary btn-sm position-absolute bottom-0 end-0 mb-2" onClick={() => eliminar(nota.id)}><i class="bi bi-trash"></i></button>
       </div>
     </div>
   );
