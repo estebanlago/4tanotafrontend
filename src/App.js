50aeaa213e6b768c8f38c0c02d3e5c70e7b9745c
estebanlago <eliazar.lagos@inacapmail.cl>
2024-07-24 07:59:07 -0400

    dsad

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
+// Importación de componentes, hooks y estilizado CSS.
 import React, { useState, useEffect } from 'react';
 import FormularioNota from './components/FormularioNota';
 import ListaNotas from './components/ListaNotas';
 import './estilo.css'
 
+// Componente principal App.
 function App() {
   const [notas, setNotas] = useState([]);
 
+  // Carga de notas almacenadas en localStorage.
+  // Actualiza el estado de las notas.
   useEffect(() => {
     const notasAlmacenadas = JSON.parse(localStorage.getItem('notas')) || [];
     setNotas(notasAlmacenadas);
   }, []);
 
+  // Almacena notas en localStorage si registra un cambio en el estado de las notas.
   useEffect(() => {
     localStorage.setItem('notas', JSON.stringify(notas));
   }, [notas]);
 
+  // Componente para agregar una nueva nota.
   const agregarNota = (nota) => {
     const nuevaNota = { ...nota, id: Date.now() }; 
     setNotas([...notas, nuevaNota]);
   };
 
+  // Componente para eliminar una nota.
   const eliminar = (id) => {
     const nuevasNotas = notas.filter(nota => nota.id !== id);
     setNotas(nuevasNotas);
   };
 
+  // Retorna estructura de la aplicación..
   return (
     <div class="container mt-5">
       <h1>Post It Simulator!</h1>
@@ -34,4 +42,5 @@ function App() {
   );
 }
 
+// Exporta componente.
 export default App;
